029d5819c114dd4155bf257f3b9edb293b657849
Volodymyr Yavorskyi <vyavorskyi@deloitte.com>
2024-08-01 15:44:22 +0300

    express

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 80;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(80);
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
+  const value = 'World-node';
   return <div>Hello {value}</div>;
 }
 
